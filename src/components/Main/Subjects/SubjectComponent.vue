6be066ed3ea9be434adcd166ddaad5eb870f7437
totarochristian <totaro.christian.18@gmail.com>
2023-04-26 16:16:09 +0200

    Add an empty subject component, install inside the subjects component and define the v-for to call all the subject from the father

---
+++ src/components/Main/Subjects/SubjectComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Subject',
+    props:{
+      data: Object
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/Main/Subjects/SubjectsComponent.vue
+++ src/components/Main/Subjects/SubjectsComponent.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="subjects" class="d-flex justify-content-center align-items-center gap-4">
-    
+    <SubjectComponent v-for="subject in store.siteCreationData.main.subjects" :data="subject"/>
   </div>
 </template>
 
 <script>
+  import SubjectComponent from './SubjectComponent.vue';
   import { store } from '../../../data/store';
 
   export default {
     name: 'Subjects',
+    components: {
+      SubjectComponent
+    },
     data(){
       return{
         store
