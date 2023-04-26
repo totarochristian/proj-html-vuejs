b8ab372e40cc0978a73f9388a6673afe8282cfc0
totarochristian <totaro.christian.18@gmail.com>
2023-04-26 13:53:11 +0200

    Define the categories data array in the store file

--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,36 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-  
+  siteCreationData:{
+    header:{
+      navbar:{
+        categories:[
+          {
+            name: 'Home',
+            link: '#'
+          },
+          {
+            name: 'Courses',
+            link: '#'
+          },
+          {
+            name: 'Instructors',
+            link: '#'
+          },
+          {
+            name: 'Events',
+            link: '#'
+          },
+          {
+            name: 'Pages',
+            link: '#'
+          },
+          {
+            name: 'Elements',
+            link: '#'
+          }
+        ]
+      }
+    }
+  }
 });
