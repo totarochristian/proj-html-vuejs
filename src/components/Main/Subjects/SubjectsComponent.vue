5b38a070e67a63de18b957ce4ff4117aefdb1136
totarochristian <totaro.christian.18@gmail.com>
2023-04-26 17:59:35 +0200

    Add flex wrap to subjects component

--- src/components/Main/Subjects/SubjectsComponent.vue
+++ src/components/Main/Subjects/SubjectsComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="subjects" class="m-4 p-4 d-flex justify-content-center align-items-center gap-4">
+  <div id="subjects" class="m-4 p-4 d-flex justify-content-center align-items-center gap-4 flex-wrap">
     <SubjectComponent v-for="subject in store.siteCreationData.main.subjects.data" :data="subject"/>
   </div>
 </template>
