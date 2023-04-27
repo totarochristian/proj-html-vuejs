39c406e3b2d293e89a7703954685f56297845e7e
totarochristian <totaro.christian.18@gmail.com>
2023-04-27 19:28:15 +0200

    Update pricing plans margins and image pos

--- src/components/Main/PricingPlans/PricingPlansComponent.vue
+++ src/components/Main/PricingPlans/PricingPlansComponent.vue
@@ -21,7 +21,7 @@
   #pricingPlans{
     background-image: url('/img/background-pattern.jpg');
     background-position: middle center;
-    padding-top: 8rem!important;
-    margin: 4rem 0 10rem 0!important;
+    padding-top: 9rem!important;
+    margin: 18rem 0 10rem 0!important;
   }
 </style>
