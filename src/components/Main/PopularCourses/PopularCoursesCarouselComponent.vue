24291a7f67bd31d6deb8cf31d49f2e59005d0804
totarochristian <totaro.christian.18@gmail.com>
2023-04-27 16:10:26 +0200

    Manage like and follower of a carousel popular courses card

--- src/components/Main/PopularCourses/PopularCoursesCarouselComponent.vue
+++ src/components/Main/PopularCourses/PopularCoursesCarouselComponent.vue
@@ -5,9 +5,27 @@
         <div class="cardHeader">
           <img :src="'/img/'+card.image" :alt="card.title">
         </div>
-        <div class="cardContent">
-          {{ index + 1 }}
+        <div class="cardContent py-4 px-4 d-flex flex-column gap-4">
+          <div class="d-flex gap-4 align-items-start justify-content-between">
+            <div class="d-flex flex-column gap-1">
+              <span class="fs-3 fw-bold">{{ card.title }}</span>
+              <span class="fs-4 colorGrey">{{ card.author }}</span>
+            </div>
+            <span class="mt-2 fs-6 px-3 badge rounded-pill text-uppercase" :class="card.price>0 ? 'text-bg-info' : 'text-bg-warning'">{{ card.price > 0 ? '$' + card.price: 'free' }}</span>
+          </div>
+          <p class="fs-4">{{ card.description }}</p>
+          <div class="d-flex gap-3">
+            <div class="d-flex gap-2 align-items-center colorGrey">
+              <i class="fa-solid fa-user"></i>
+              <span>{{ card.follower }}</span>
+            </div>
+            <div class="d-flex gap-2 align-items-center colorGrey">
+              <i class="fa-solid fa-tag"></i>
+              <span class="text-uppercase">{{ card.tag }}</span>
+            </div>
+          </div>
         </div>
+        <i class="cardHearth p-4 fs-3 fa-heart" :class="card.liked ? 'fa-solid text-danger' : 'fa-regular text-white'" @click="UpdateLike(index)"></i>
       </div>
     </div>
     <div id="carouselButtons" class="w-100 d-flex justify-content-center align-items-center gap-3">
@@ -42,6 +60,10 @@
         this.startIndex = indexToOpen - 1;
         this.NextSlide();
         this.interval = setInterval(this.NextSlide,4000);
+      },
+      UpdateLike(index){
+        store.siteCreationData.main.popularCourses.data[index].follower += store.siteCreationData.main.popularCourses.data[index].liked ? -1 : 1;
+        store.siteCreationData.main.popularCourses.data[index].liked = !store.siteCreationData.main.popularCourses.data[index].liked;
       }
     },
     mounted(){
@@ -54,6 +76,8 @@
   @use '../../../assets/styles/partial/variables' as *;
   .coursesCard{
     max-width: 413px;
+    border: 1px solid lightgrey;
+    position: relative;
     .cardHeader{
       width: 100%;
       max-height: 265px;
@@ -62,6 +86,22 @@
         height: 100%;
         object-fit: cover;
       }
+    }
+    .badge{
+      width: 70px;
+    }
+    .colorGrey{
+      color: grey;
+    }
+    .cardHearth{
+      position: absolute;
+      top: 0;
+      right: 0;
+      display: none;
+      cursor: pointer;
+    }
+    &:hover .cardHearth{
+      display: block;
     }
   }
   #carouselButtons{
