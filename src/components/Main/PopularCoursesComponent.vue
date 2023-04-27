e58a3c54bb88ef64932d6792d00d8979da613901
totarochristian <totaro.christian.18@gmail.com>
2023-04-27 14:06:10 +0200

    Define title and subtitle of the popular online courses component

--- src/components/Main/PopularCoursesComponent.vue
+++ src/components/Main/PopularCoursesComponent.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Popular Courses
+  <div id="popularOnlineCourses" class="m-4 p-4 d-flex justify-content-center align-items-center flex-column gap-4">
+    <h2 class="fs-1 text-capitalize">Popular online courses</h2>
+    <p class="fs-3 col-xl-6 text-center">Lorem ipsum gravida nibh vel velit auctor aliquetnean sollicitudin, lorem quis bibendum auci elit consequat ipsutis sem nibh id elit</p>
   </div>
 </template>
 
@@ -11,5 +12,7 @@
 </script>
 
 <style lang="scss" scoped>
+  #popularOnlineCourses{
 
+  }
 </style>
