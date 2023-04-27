19a6488e8df52f25d4f03e63d517a1c9f3488edc
totarochristian <totaro.christian.18@gmail.com>
2023-04-27 18:52:06 +0200

    Update the font-sizes in the pricing table components

--- src/components/Main/PricingPlans/PricingTableComponent.vue
+++ src/components/Main/PricingPlans/PricingTableComponent.vue
@@ -6,16 +6,16 @@
           <th scope="col" v-for="(header, index) in store.siteCreationData.main.pricingTable.headers" class="col-3 p-4" :class="{'bg-azure': index>0}">
             <div class="px-2 py-4 d-flex flex-column justify-content-center align-items-center gap-3">
               <img :src="'/img/' + header.image" :alt="header.title" v-if="header?.image">
-              <h2 class="fs-3">{{ header.title }}</h2>
-              <span class="fs-4" v-if="header?.price">${{ header.price }}</span>
+              <h2 class="fs-1">{{ header.title }}</h2>
+              <span class="fs-2" v-if="header?.price">${{ header.price }}</span>
             </div>
           </th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="row in store.siteCreationData.main.pricingTable.rows">
-          <th scope="row" class="fs-5 py-3 px-4 text-grey">{{ row.title }}</th>
-          <td v-for="value in row.values" class="py-3 px-4 fs-4 text-center text-grey">
+          <th scope="row" class="fs-4 py-3 px-4 text-grey">{{ row.title }}</th>
+          <td v-for="value in row.values" class="py-3 px-4 fs-3 text-center text-grey">
             <span v-if="row.type == 'number'">{{ value }}</span>
             <span v-if="row.type == 'time'">{{ value }} Days</span>
             <i class="fa-solid" :class="value ? 'fa-check text-info' : 'fa-xmark'" v-if="row.type == 'boolean'"></i>
