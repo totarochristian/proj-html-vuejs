d9fc19ed9ed20a031d7367958085f988e1dd08e8
totarochristian <totaro.christian.18@gmail.com>
2023-04-27 14:47:42 +0200

    Prepare the card header of the courses card in the popular courses carousel component and show only 3 elements of them

--- src/components/Main/PopularCourses/PopularCoursesCarouselComponent.vue
+++ src/components/Main/PopularCourses/PopularCoursesCarouselComponent.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="popularCoursesCarousel">
-    
+  <div id="popularCoursesCarousel" class="d-flex justify-content-center align-items-center gap-4">
+    <div class="coursesCard d-flex flex-column gap-4" v-for="(card,index) in store.siteCreationData.main.popularCourses.data" :class="{'d-none': index < startIndex || index > endIndex}">
+      <div class="cardHeader">
+        <img :src="'/img/'+card.image" :alt="card.title">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,12 +15,25 @@
     name: 'PopularCoursesCarousel',
     data(){
       return{
-        store
+        store,
+        startIndex: 0,
+        endIndex: 2
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-
+  .coursesCard{
+    max-width: 413px;
+    .cardHeader{
+      width: 100%;
+      max-height: 265px;
+      img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+  }
 </style>
