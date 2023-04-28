fc9c6fc79b90edff0c45f6fd11cb898d1230baa4
totarochristian <totaro.christian.18@gmail.com>
2023-04-28 10:48:51 +0200

    Add style in the upper footer

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -1,41 +1,41 @@
 <template>
-  <footer class="bg-dark text-white d-flex flex-column gap-4">
-    <div class="d-flex justify-content-between align-items-center gap-4">
-      <div class="d-flex justify-content-between align-items-center gap-4">
+  <footer class="py-4 bg-dark text-white d-flex flex-column gap-4">
+    <div id="upperFooter" class="p-4 d-flex justify-content-between align-items-start gap-4">
+      <div class="p-4 d-flex justify-content-between align-items-center gap-4">
         <div id="generalInfo" class="col-3 d-flex flex-column align-items-start justify-content-center gap-4">
           <img :src="'/img/' + store.siteCreationData.footer.upper.general.image" alt="Logo">
-          <p>{{ store.siteCreationData.footer.upper.general.description }}</p>
-          <div class="d-flex align-items-center gap-3">
+          <p class="fs-4">{{ store.siteCreationData.footer.upper.general.description }}</p>
+          <div class="fs-4 d-flex align-items-center gap-3">
             <i class="fa-solid fa-phone-volume"></i>
             <span>{{ store.siteCreationData.footer.upper.general.phone }}</span>
           </div>
-          <div class="d-flex align-items-center gap-3">
+          <div class="fs-4 d-flex align-items-center gap-3">
             <i class="fa-regular fa-clock"></i>
             <span>{{ store.siteCreationData.footer.upper.general.hours }}</span>
           </div>
         </div>
         <div id="popularCoursesInfo" class="col-3 d-flex flex-column align-items-start justify-content-center gap-4">
-          <h3>{{ store.siteCreationData.footer.upper.popularCourses.title }}</h3>
+          <h3 class="fs-2">{{ store.siteCreationData.footer.upper.popularCourses.title }}</h3>
           <div class="d-flex flex-column align-items-start justify-content-center gap-3">
             <div class="d-flex flex-column align-items-start justify-content-center gap-1" v-for="info in store.siteCreationData.footer.upper.popularCourses.data">
-              <a :href="info.titleLink">{{ info.title }}</a>
-              <a :href="info.authorLink">{{ info.author }}</a>
+              <a :href="info.titleLink" class="fs-4 fw-seminold text-capitalize">{{ info.title }}</a>
+              <a :href="info.authorLink" class="fs-5 text-capitalize">{{ info.author }}</a>
             </div>
           </div>
         </div>
         <div id="supportInfo" class="col-3 d-flex flex-column align-items-start justify-content-center gap-4">
-          <h3>{{ store.siteCreationData.footer.upper.support.title }}</h3>
+          <h3 class="fs-2">{{ store.siteCreationData.footer.upper.support.title }}</h3>
           <div class="d-flex flex-column align-items-start justify-content-center gap-3">
-            <a :href="info.link" v-for="info in store.siteCreationData.footer.upper.support.data">{{ info.title }}</a>
+            <a :href="info.link" class="fs-4" v-for="info in store.siteCreationData.footer.upper.support.data">{{ info.title }}</a>
           </div>
         </div>
       </div>
-      <div id="flexibleLearnings" class="col-3 d-flex flex-column align-items-start justify-content-center gap-4">
-        <h3>{{ store.siteCreationData.footer.upper.flexibleLearning.title }}</h3>
+      <div id="flexibleLearnings" class="p-4 col-3 d-flex flex-column align-items-start justify-content-center gap-4">
+        <h3 class="fs-2">{{ store.siteCreationData.footer.upper.flexibleLearning.title }}</h3>
         <img :src="'/img/' + store.siteCreationData.footer.upper.flexibleLearning.image" alt="World image">
       </div>
     </div>
-    <div class="d-flex justify-content-between align-items-center gap-4">
+    <div id="bottomFooter" class="d-flex justify-content-between align-items-center gap-4">
       <div>
         copyright
       </div>
@@ -60,5 +60,36 @@
 </script>
 
 <style lang="scss" scoped>
-
+  @use '../../assets/styles/partial/variables' as *;
+  footer{
+    padding-top: 6rem!important;
+    a{
+      color: inherit;
+      text-decoration: none;
+      &:hover{
+        color: $buttonColor1;
+      }
+    }
+    #popularCoursesInfo a:last-child:hover{
+      color: grey;
+    }
+    #generalInfo>div:hover i{
+      color: $buttonColor1;
+      cursor: pointer;
+    }
+    #upperFooter{
+      #generalInfo{
+        img{
+          height: 3.5rem;
+          width: auto;
+        }
+      }
+      #flexibleLearnings{
+        img{
+          width: 305px;
+          height: auto;
+        }
+      }
+    }
+  }
 </style>
