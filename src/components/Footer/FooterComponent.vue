99f6ff310e3333759df88badf2c3cb774c78ee22
totarochristian <totaro.christian.18@gmail.com>
2023-04-28 10:14:05 +0200

    Add basic content in the first section of the upper footer

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -1,11 +1,28 @@
 <template>
-  <footer class="d-flex flex-column gap-4">
+  <footer class="bg-dark text-white d-flex flex-column gap-4">
     <div class="d-flex justify-content-between align-items-center gap-4">
-      <div>
-        Sections
+      <div class="d-flex justify-content-between align-items-center gap-4">
+        <div id="generalInfo" class="d-flex flex-column align-items-start justify-content-center gap-4">
+          <img :src="'/img/' + store.siteCreationData.footer.upper.general.image" alt="Logo">
+          <p>{{ store.siteCreationData.footer.upper.general.description }}</p>
+          <div class="d-flex align-items-center gap-3">
+            <i class="fa-solid fa-phone-volume"></i>
+            <span>{{ store.siteCreationData.footer.upper.general.phone }}</span>
+          </div>
+          <div class="d-flex align-items-center gap-3">
+            <i class="fa-regular fa-clock"></i>
+            <span>{{ store.siteCreationData.footer.upper.general.hours }}</span>
+          </div>
+        </div>
+        <div id="popularCoursesInfo">
+          
+        </div>
+        <div id="supportInfo">
+          
+        </div>
       </div>
-      <div>
-        Image
+      <div id="flexibleLearnings">
+
       </div>
     </div>
     <div class="d-flex justify-content-between align-items-center gap-4">
