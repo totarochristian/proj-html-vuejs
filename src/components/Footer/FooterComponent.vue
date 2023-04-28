6ed9e10883dbb4a73dd84455076586720f4dab29
totarochristian <totaro.christian.18@gmail.com>
2023-04-28 10:06:30 +0200

    Define the inner basic structure for both upper and bottom footer

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -1,10 +1,20 @@
 <template>
   <footer class="d-flex flex-column gap-4">
-    <div class="d-flex justify-content-center align-items-center gap-4">
-      Upper
+    <div class="d-flex justify-content-between align-items-center gap-4">
+      <div>
+        Sections
+      </div>
+      <div>
+        Image
+      </div>
     </div>
-    <div class="d-flex justify-content-center align-items-center gap-4">
-      Bottom
+    <div class="d-flex justify-content-between align-items-center gap-4">
+      <div>
+        copyright
+      </div>
+      <div>
+        follow
+      </div>
     </div>
   </footer>
 </template>
