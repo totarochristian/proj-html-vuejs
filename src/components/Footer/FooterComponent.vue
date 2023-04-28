a50420962f4c5c7383ed475626a331abb558a3d1
totarochristian <totaro.christian.18@gmail.com>
2023-04-28 10:03:21 +0200

    Define the inner major structure of the footer (upper and bottom footer div

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -1,12 +1,24 @@
 <template>
-  <footer>
-    Footer
+  <footer class="d-flex flex-column gap-4">
+    <div class="d-flex justify-content-center align-items-center gap-4">
+      Upper
+    </div>
+    <div class="d-flex justify-content-center align-items-center gap-4">
+      Bottom
+    </div>
   </footer>
 </template>
 
 <script>
+  import { store } from '../../data/store';
+
   export default {
-    name: 'Footer'
+    name: 'Footer',
+    data(){
+      return{
+        store
+      }
+    }
   }
 </script>
 
